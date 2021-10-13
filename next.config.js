a4058b09aeba0faf1ca21c9604937b8b249fca22
kyle-makelog <kyle@makelog.io>
2021-10-13 09:22:37 -0600

    fix: prisma SSR build issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // https://github.com/prisma/prisma/issues/6899
+      config.externals.push("_http_common");
+    }
+    return config;
+  },
+};
